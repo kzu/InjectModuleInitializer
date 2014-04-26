148b3687b7aa59c0d052718aee002c5546ffd942
Einar Egilsson <einar@einaregilsson.com>
2014-04-26 22:43:49 +0200

    Change license to MIT, delete .exe

--- Injector.cs
+++ Injector.cs
@@ -6,19 +6,8 @@ Command line program to inject a module initializer into a .NET assembly.
 Copyright (C) 2009-2012 Einar Egilsson
 http://einaregilsson.com/module-initializers-in-csharp/
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+This program is licensed under the MIT license: http://opensource.org/licenses/MIT
+ */
 using System;
 using System.Diagnostics;
 using System.IO;
