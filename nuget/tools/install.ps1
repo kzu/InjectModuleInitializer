6636220b214d237b47857593b0ed51ccb8df0799
Brian Friesen <brianfriesen@quickenloans.com>
2014-04-28 17:10:02 -0400

    If ModuleInitializer.cs exists in another location, delete the template.

--- nuget/tools/install.ps1
+++ nuget/tools/install.ps1
@@ -1,6 +1,6 @@
 param($installPath, $toolsPath, $package, $project)
 
-Function Get-Relative-Path ($from, $to)
+function Get-Relative-Path ($from, $to)
 {
 	$path = $NULL
 
@@ -34,6 +34,18 @@ Function Get-Relative-Path ($from, $to)
 	$path
 }
 
+function Check-ModuleInitializer($buildProject)
+{
+	$moduleInitializers = $buildProject.Items | where {$_.Xml.ItemType -eq "Compile" -and $_.Xml.Include.EndsWith("ModuleInitializer.cs")}
+	$count = ($moduleInitializers | measure).Count
+	
+	if ($count -gt 1)
+	{
+		Write-Host "ModuleInitializer.cs already exists in another location. Deleting template..."
+		$project.ProjectItems.Item("ModuleInitializer.cs").Delete()
+	}
+}
+
 $relativePath = Get-Relative-Path $project.FileName $toolsPath
 
 Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
@@ -43,5 +55,7 @@ $target = $buildProject.Xml.AddTarget("InjectModuleInitializer")
 $target.AfterTargets = "AfterBuild"
 $task = $target.AddTask("Exec")
 $task.SetParameter("Command", $relativePath + "InjectModuleInitializer.exe `"`$(TargetPath)`"")
+
+Check-ModuleInitializer($buildProject)
 
 $project.Save()
